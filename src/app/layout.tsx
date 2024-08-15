bb273ff965bf7846872fc08911ece62813c5581f
EmJhey <emjheypb@gmail.com>
2024-08-15 18:07:43 -0400

    frames

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SBMI",
   description: "Sumido Blockchain Mining Inc.",
+  // provide a full URL to your /frames endpoint
+  other: {
+    "fc:frame": "vNext",
+    "fc:frame:image": `https://${process.env.NEXT_PUBLIC_SITE_URL}/images/logo-transparent.png`,
+    "fc:frame:image:aspect_ratio": "1:1",
+    "fc:frame:button:1": `Learn More`,
+    "fc:frame:button:1:link": `https://${process.env.NEXT_PUBLIC_SITE_URL}/`,
+  },
 };
 
 export default function RootLayout({
