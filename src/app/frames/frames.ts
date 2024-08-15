d72fa9c0995094d90fa99efe2a55e1c015e4f44f
EmJhey <emjheypb@gmail.com>
2024-08-15 18:08:05 -0400

    cleanup

--- src/app/frames/frames.ts
+++
@@ -1,7 +0,0 @@
-import { farcasterHubContext } from "frames.js/middleware";
-import { createFrames } from "frames.js/next";
-
-export const frames = createFrames({
-  basePath: "/frames",
-  middleware: [farcasterHubContext()],
-});
